35324fae6e08350fef2bb54de364681f6e20ab42
Leo Simmons <leooscar.simmons@gmail.com>
2017-01-06 14:09:23 -0500

    hashing with bcryptjs

--- src/server/auth/controller.js
+++ src/server/auth/controller.js
@@ -1,24 +1,26 @@
 
 import express from 'express'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 const pgp = require('pg-promise')();
 import db from '../db'
 
 
-// console.log(req.body);
-// let hashes = {};
-// bcrypt.genSalt(15, (err, salt1) => {
-//   bcrypt.hash(req.body.password1, salt1, (err, hash1) => {
-//     bcrypt.genSalt(15, (err, salt2) => {
-//       bcrypt.hash(req.body.password2, salt1, (err, hash2) => {
-//         return res.send({
-//           hash1,
-//           hash2
-//         })
-//       })
-//     })
-//   })
-// })
+function hashPass(password){
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err){
+        return reject(err);
+      }
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err){
+          return reject(err);
+        }
+        return resolve({ salt, hash });
+      })
+    })
+  })
+}
+
 
 function findUser(username, email){
   return new Promise((resolve, reject) => {
@@ -33,16 +35,27 @@ function findUser(username, email){
   })
 }
 
-function createUser(username, email, firstname, lastname){
-  return new Promise((resolve, reject) => {
-    let queryString = 'INSERT INTO "Users"(firstname, lastname, username, email) values($1, $2, $3, $4) returning id';
-    db.one(queryString, [firstname, lastname, username, email])
-    .then(data => {
-      return resolve(data.id);
-    })
-    .catch(err => {
-      return reject(err);
-    })
+function createUser(req, res){
+  let username = req.body.username;
+  let email = req.body.email;
+  let firstname = req.body.firstname;
+  let lastname = req.body.lastname;
+  let password = req.body.password;
+  hashPass(password)
+  .then( passData => {
+    let salt = passData.salt;
+    let hash = passData.hash;
+    let queryString = 'INSERT INTO "Users"(firstname, lastname, username, email, salt, password) values($1, $2, $3, $4, $5, $6) returning id';
+    return db.one(queryString, [firstname, lastname, username, email, salt, hash])
+  })
+  .then( data => {
+    return res.status(200).json({
+      message: 'User created',
+      id: data.id
+    });
+  })
+  .catch( err => {
+    return res.status(500).send(err);
   })
 }
 
@@ -50,22 +63,14 @@ function createUser(username, email, firstname, lastname){
 function signUp(req, res, next){
   let username = req.body.username;
   let email = req.body.email;
-  let firstname = req.body.firstname;
-  let lastname = req.body.lastname;
   findUser(username, email)
     .then( user => {
       if (user){
         // user already exists - check password and
         // either log in or prompt for password again
-        return res.status(302).send();
+        return res.status(302).send('User exists already');
       }
-      return createUser(username, email, firstname, lastname)
-    })
-    .then( user => {
-      return res.status(200).json({
-        message: 'user created',
-        user
-      })
+      return createUser(req, res);
     })
     .catch( err => {
       res.status(500).send(err);

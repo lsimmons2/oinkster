c9017a9e353ccdecbe3da1634ef73b913d8689e8
Leo Simmons <leooscar.simmons@gmail.com>
2017-01-11 15:06:33 -0500

    async before hook for db seeding

--- test/server/oinks/controller.spec.js
+++ test/server/oinks/controller.spec.js
@@ -15,20 +15,48 @@ const knex = require('knex')(knexConfig);
 
 
 
-describe('getOinks()', function() {
+describe('getOinks()', function(done) {
 
-  before( function() {
-    knex.seed.run({directory: './db/seeds/test'})
+  before( function(done) {
+    knex.seed.run({
+      directory: './db/seeds/test'
+    })
+      .then( () => {
+        done();
+      })
   })
 
   it('should return all oinks in db', function(done) {
     agent
       .get('/oinks')
-      .expect(201)
       .end(function(err, res) {
+        let status = res.status;
         let oinks = res.body;
-        oinks.length.should.equal(3);
+        status.should.equal(200)
+        oinks.length.should.equal(4);
         done();
       })
   })
+
+  it('should return oinks with all properties', function(done) {
+    agent
+      .get('/oinks')
+      .end( function(err, res) {
+        let status = res.status;
+        let oinks = res.body;
+        status.should.equal(200);
+        oinks.forEach( oink => {
+          oink.id.should.be.a('string');
+          oink.text.should.be.a('string');
+          if (oink.asset){
+            oink.asset.should.be.a('string');
+          } else {
+            should.equal(oink.asset, null);
+          }
+          oink.user.should.be.a('string');
+        })
+        done();
+      })
+  })
+
 })

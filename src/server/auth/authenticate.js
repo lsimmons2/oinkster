ab2ad214907bc0690fdd9427edaa91691536ad24
Leo Simmons <leooscar.simmons@gmail.com>
2017-01-12 16:29:50 -0500

    check for presence of authorization header before checking contents

--- src/server/auth/authenticate.js
+++ src/server/auth/authenticate.js
@@ -3,13 +3,18 @@ import jwt from 'jsonwebtoken'
 
 
 export default function authenticate(req, res, next){
+  if (req.headers['authorization'] === undefined){
+    return res.status(400).json({
+      message: 'nah'
+    });
+  }
   let token = req.headers['authorization'].replace('Bearer ', '');
   jwt.verify(token, 'sah', (err, user) => {
     if (err){
       return res.status(403).json({
-        message: 'cant get through me',
+        message: 'Invalid JWT',
         err
-      })
+      });
     }
     req.user = user;
     next();

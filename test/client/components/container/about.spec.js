f82121bb7ff20c9180a2ca02b8447aa2b8c57aa3
Leo Simmons <leooscar.simmons@gmail.com>
2017-01-12 15:10:32 -0500

    inital setup for testing container components with jsdom and enzyme

---
+++ test/client/components/container/about.spec.js
@@ -0,0 +1,35 @@
+
+// jsdom for DOM APIs, lifecycle methods, use of mount()
+require('./dom')('<html><body></body></html>');
+
+import React from 'react'
+import { Provider } from 'react-redux';
+import chai from 'chai'
+import { mount } from 'enzyme'
+
+const should = chai.should();
+
+import About from '../../../../src/client/components/about'
+import initialState from '../../../../src/client/initial-state'
+import mockStore from './mock-store'
+
+
+describe('<About/>', () => {
+
+  let Component;
+
+  beforeEach(() => {
+    const store = mockStore(initialState);
+    const wrapper = mount(
+      <Provider store={store}>
+        <About/>
+      </Provider>
+    );
+    Component = wrapper.find(About);
+  });
+
+  it('Should render as div', () => {
+    Component.length.should.be.ok;
+  });
+
+});

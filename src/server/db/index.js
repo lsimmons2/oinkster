7cbc1db3b01011cf012f9275c8b9c0c444802bd0
Leo Simmons <leooscar.simmons@gmail.com>
2017-01-16 18:47:27 -0500

    get db from env

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,13 +1,9 @@
 
 const pgp = require('pg-promise')();
-const env = process.env.NODE_ENV;
 import config from '../../../config/db-config'
 
-let database = 'oinkster-' + env;
 
-let cn = config.connection;
-cn['database'] = database;
-
+const cn = config.connection;
 const db = pgp(cn);
 
 
